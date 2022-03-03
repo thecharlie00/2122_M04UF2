afa5efe606a657df4e402101ec41edc1d55ec5aa
enti <enti@enti.enti>
2022-03-03 19:08:58 +0100

    PLANTILLA REACT

---
+++ ejemplo_react/src/Piesito.js
@@ -0,0 +1,20 @@
+import './Piesito.css';
+
+function Piesito(){
+
+
+    return(
+       <footer>
+
+       <p>Copy(c)2022 Plantilla React/p>
+	   </footer>
+
+  
+
+
+);
+
+
+}
+
+export default Piesito;
