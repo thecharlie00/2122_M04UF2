bce0c8a911c6ceed5bbc99b38c7fa91e571b9877
enti <enti@enti.enti>
2022-03-09 20:30:24 +0100

    Relojito

--- ejemplo_react/src/Piesito.js
+++ ejemplo_react/src/Piesito.js
@@ -1,18 +1,42 @@
 import './Piesito.css';
+import React from 'react';
 
+class Piesito extends React.Component
 function Piesito(){
+	constructor(props){
+		super(props);
+
+		this.state = {
+			date: new Date()
+		};
+
+		setInterval( () => {
+			this.setState({
+				date: new Date()
+			});
+		}, 1000);
+
+	}
 
 
-    return(
-       <footer>
+	render(){
 
-       <p>Copy(c)2022 Plantilla React/p>
-	   </footer>
+		return(
+			<footer>
+				<p>Copy (c)2022 Copyright malo</p>
+				<p>{this.state.date.toLocaleTimeSring()}</p>
+			</footer>
+           
+
+		);
+
+	}
+
 
   
 
 
-);
+
 
 
 }
